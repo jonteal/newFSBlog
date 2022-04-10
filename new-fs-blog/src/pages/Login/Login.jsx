36e10388f072a1611458ed8decda509f51525a33
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-10 18:23:43 -0500

    Basic Login page without style constructed

--- new-fs-blog/src/pages/Login/Login.jsx
+++ new-fs-blog/src/pages/Login/Login.jsx
@@ -2,8 +2,16 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='login'>
+            <form className="loginForm">
+                <label>Email</label>
+                <input type="text" placeholder='Enter your email...' />
+                <label>Password</label>
+                <input type="password" placeholder='Enter your password...' />
+                <button className="loginButton">Login</button>
+
+            </form>
+            <button className="loginRegisterButton">Register</button>
         </div>
     );
 }
