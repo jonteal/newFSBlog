d28bbb76cf08055c4cdc3483917e90919b4459e3
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-10 17:43:27 -0500

    Write Post component styled and mostly done. Needs Img src

--- new-fs-blog/src/pages/Write/Write.jsx
+++ new-fs-blog/src/pages/Write/Write.jsx
@@ -1,9 +1,22 @@
 import './Write.css';
+import { RiAddFill } from "react-icons/ri";
 
 const Write = () => {
     return (
         <div className="write">
-            Write
+            <form className='writeForm'>
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                    < RiAddFill className="writeIcon" />
+                </label>
+                <input type="file" id='fileInput' style={{display: "none"}} />
+                <input type="text" placeholder='Title' className='writeInput' autoFocus={true} />
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder='Tell your story...' type='text' className='writeInput writeText'></textarea>
+            </div>
+            <button className='writeSubmit'>Publish</button>
+            </form>
         </div>
     );
 }
