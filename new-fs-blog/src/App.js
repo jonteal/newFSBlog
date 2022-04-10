f70228600b3cc9d2ce1b5204d1c23c0ed3937044
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-10 17:58:48 -0500

    Initiated Settings page

--- new-fs-blog/src/App.js
+++ new-fs-blog/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Topbar from './components/Topbar/Topbar';
 import Home from './pages/Home/Home';
+import Settings from './pages/Settings/Settings';
 import Single from './pages/Single/Single';
 import Write from './pages/Write/Write';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Topbar />
-      <Write />
+      <Settings />
     </div>
   );
 }
---
+++ new-fs-blog/src/pages/Settings/Settings.jsx
@@ -0,0 +1,13 @@
+import './Settings.css';
+import Sidebar from '../../components/Sidebar/Sidebar';
+
+const Settings = () => {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper"></div>
+            <Sidebar />
+        </div>
+    );
+}
+
+export default Settings;
