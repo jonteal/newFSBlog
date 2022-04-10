120f389c954b34a8c75393655aaa2e1817fef747
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-10 12:04:13 -0500

    Initiating single post page

--- new-fs-blog/src/App.js
+++ new-fs-blog/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Topbar from './components/Topbar/Topbar';
 import Home from './pages/Home/Home';
+import Single from './pages/Single/Single';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Home />
+      <Single />
     </div>
   );
 }
---
+++ new-fs-blog/src/pages/Single/Single.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/Sidebar/Sidebar";
+import "./Single.css";
+
+const Single = () => {
+    return (
+        <div className="single">
+            {/* post */}
+            <Sidebar />
+        </div>
+    );
+}
+
+export default Single;
