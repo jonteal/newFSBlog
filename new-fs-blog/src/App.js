0ae9ebcf7633b54147c1438a3e4013f67df7a183
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-10 12:33:24 -0500

    Write page initiated

--- new-fs-blog/src/App.js
+++ new-fs-blog/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Topbar from './components/Topbar/Topbar';
 import Home from './pages/Home/Home';
 import Single from './pages/Single/Single';
+import Write from './pages/Write/Write';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Single />
+      <Write />
     </div>
   );
 }
---
+++ new-fs-blog/src/pages/Write/Write.jsx
@@ -0,0 +1,11 @@
+import './Write.css';
+
+const Write = () => {
+    return (
+        <div className="write">
+            Write
+        </div>
+    );
+}
+
+export default Write;
