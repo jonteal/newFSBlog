7fbbdb3f1b362ada57d379b2f25d8b8d9f3200f5
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-10 18:21:34 -0500

    Initiated Login form and Login CSS

--- new-fs-blog/src/App.js
+++ new-fs-blog/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Topbar from './components/Topbar/Topbar';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 import Settings from './pages/Settings/Settings';
 import Single from './pages/Single/Single';
 import Write from './pages/Write/Write';
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Topbar />
-      <Settings />
+      <Login />
     </div>
   );
 }
