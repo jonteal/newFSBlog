5e550084c4894eb3ffc7dee54fd6f08f63197c2a
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-16 13:40:31 -0500

    Initialized useState and useEffect to create fetching method using axios

--- new-fs-blog/client/src/pages/Home/Home.jsx
+++ new-fs-blog/client/src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 // import Sidebar from "../../Sidebar/Sidebar";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Posts from "../../components/Posts/Posts";
 
 const Home = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("/posts")
+            console.log(res);
+        } 
+    }, [])
+
     return (
         <>
         <Header />
