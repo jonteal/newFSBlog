74b4c0fe279e2e30152edcb5badbcfa6d21493c1
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-16 15:26:48 -0500

    Connected to MongoDB db with dotenv file

--- new-fs-blog/api/index.js
+++ new-fs-blog/api/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(console.log("Connected to MongoDB!"))
+.catch((err) => console.log(err));
 
 
 app.listen('5000', () => {
