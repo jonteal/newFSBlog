21cff7a233a4b96a31a083cd40b0aad09bc573ba
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-17 09:54:06 -0500

    Added status 200 response if sign up post is successful

--- new-fs-blog/api/routes/auth.js
+++ new-fs-blog/api/routes/auth.js
@@ -11,6 +11,7 @@ router.post('/signup', async (req, res)) {
         })
 
         const user = await newUser.save();
+        res.status(200).json(user);
     } catch(err) {
         res.status(500).json(err);
     }
