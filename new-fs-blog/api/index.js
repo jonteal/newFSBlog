fa18282eea9ae7483c8fb10d6e43b03882340e04
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-22 07:01:50 -0500

    Added bcrypt to user update method and route

--- new-fs-blog/api/index.js
+++ new-fs-blog/api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+
 
 dotenv.config();
 app.use(express.json());
@@ -17,7 +19,7 @@ mongoose
     .catch((err) => console.log(err));
 
 app.use('/api/auth', authRoute);
-
+app.use('/api/auth', userRoute);
 
 app.listen('5000', () => {
     console.log("Backend is running!");
--- new-fs-blog/api/routes/users.js
+++ new-fs-blog/api/routes/users.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // UPDATE
 router.put('/:id', async (req,res) => {
     if(req.body.userId === req.params.id) {
+        if(req.body.password) {
+            const sale = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
         try {
-        
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body, 
+            });
+            res.status(200).json(updatedUser);
         } catch(err) {
             res.status(500).json(err);
         }
