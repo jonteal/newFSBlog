ee606614d9874b5c5279ac11a77d8a198c0c85f0
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-24 06:40:06 -0500

    Delete Post operation is working and removing from db

--- new-fs-blog/api/routes/posts.js
+++ new-fs-blog/api/routes/posts.js
@@ -27,7 +27,7 @@ router.put('/:id', async (req,res) => {
                 })
                 res.status(200).json(updatedPost);
             } catch (err) {
-                res.status(401).json("You can only update your posts!")
+                res.status(401).json(err)
             }
         } else {
             res.status(401).json("You can only update your posts!");
@@ -40,7 +40,25 @@ router.put('/:id', async (req,res) => {
 });
 
 // DELETE POST
+router.delete('/:id', async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.username === req.body.username) {
+            try {
+                await post.delete();
+                res.status(200).json("Post has been deleted!");
+            } catch (err) {
+                res.status(401).json(err)
+            }
+        } else {
+            res.status(401).json("You can only delete your posts!");
+        }
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
+    
+});
 
 // GET POST
 router.get("/:id", async (req, res) => {
