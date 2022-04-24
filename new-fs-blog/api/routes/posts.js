36209c326cd85edf2b20edac211ef9a3206ac025
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-24 02:49:29 -0500

    Post method for creating posts is built with updated routes in index.js

--- new-fs-blog/api/routes/posts.js
+++ new-fs-blog/api/routes/posts.js
@@ -7,6 +7,7 @@ router.post('/', async (req,res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = newPost.save();
+        res.status(200).json(savedPost);
     } catch (err) {
         res.status(500).json(err);
     }
