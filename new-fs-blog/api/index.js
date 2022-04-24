6897ab00f480e6b03ac48dbdf5cf002f9b9ca70d
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-24 12:09:24 -0500

    Create Category is working and saving to the db

--- new-fs-blog/api/index.js
+++ new-fs-blog/api/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
-
+const categoryRoute = require('./routes/categories');
 
 dotenv.config();
 app.use(express.json());
@@ -22,6 +22,8 @@ mongoose
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
+
 
 
 app.listen('5000', () => {
