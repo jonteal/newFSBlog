2b8bffdf96a1aca5b9cdfa8a740a71f56379f135
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-24 20:25:45 -0500

    Signup form is sending data object succesfully

--- new-fs-blog/client/src/pages/Signup/Signup.jsx
+++ new-fs-blog/client/src/pages/Signup/Signup.jsx
@@ -1,19 +1,54 @@
 import './Signup.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 
 const Signup = () => {
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await axios.post('/auth/signup', {
+            username, 
+            email, 
+            password,
+        });
+        console.log(res);
+
+    }
+
+
     return (
         <div className='signup'>
             <span className="signupTitle">Signup</span>
-            <form className="signupForm">
+            <form className="signupForm" onSubmit={handleSubmit}>
                 <label>Username</label>
-                <input type="text" className='signupInput' placeholder='Enter your username...' />
+                <input 
+                    type="text" 
+                    className='signupInput' 
+                    placeholder='Enter your username...' 
+                    onChange={e => setUsername(e.target.value)}
+                />
+
                 <label>Email</label>
-                <input type="text" className='signupInput' placeholder='Enter your email...' />
+                <input 
+                    type="text" 
+                    className='signupInput' 
+                    placeholder='Enter your email...' 
+                    onChange={e => setEmail(e.target.value)}
+                />
                 <label>Password</label>
-                <input type="password" className='signupInput' placeholder='Enter your password...' />
-                <button className="signupButton">Signup</button>
+                <input 
+                    type="password" 
+                    className='signupInput' 
+                    placeholder='Enter your password...' 
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button className="signupButton" type='submit'>Signup</button>
 
             </form>
             <button className="loginButton">
