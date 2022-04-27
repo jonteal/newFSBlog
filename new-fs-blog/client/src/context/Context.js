0578ea94dc53cf8f0f0fb8f38340caf3da54dc12
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-04-26 20:43:05 -0500

    handleSubmit for Login form is using context now

--- new-fs-blog/client/src/context/Context.js
+++ new-fs-blog/client/src/context/Context.js
@@ -14,11 +14,12 @@ export const ContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
 
     return(
-        <Context.Provider value={{
-            user:state.user,
-            isFetching:state.isFetching,
-            error: state.error,
-            dispatch,
+        <Context.Provider 
+            value={{
+                user:state.user,
+                isFetching:state.isFetching,
+                error: state.error,
+                dispatch,
         }}>
             {children}
         </Context.Provider>
